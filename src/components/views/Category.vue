78acf6b933f4a6f8e12d2c22d0d2be14658ee0e7
Kniffen <knifftech@gmail.com>
2019-02-02 17:15:36 +0100

    Added ability to follow/unfollow categories

--- src/components/views/Category.vue
+++ src/components/views/Category.vue
@@ -2,9 +2,22 @@
 
   <Loading v-if="isLoading" />
 
-  <div id="category" v-else>
+  <div class="category" v-else>
   
-    <h1><CategoryIcon v-bind:name="title" />{{title}}</h1>
+    <header>
+      <span class="title">
+        <CategoryIcon v-bind:name="category.name" />{{category.name}}
+      </span>
+
+      <div class="category-menu" v-if="username && category.id">
+        <button 
+          class="cta" 
+          @click="toggleFollow"
+          @mouseover="mouseOver"
+          @mouseout="mouseOut"
+        >{{followBtnText}}</button>
+      </div>
+    </header>
 
     <div v-if="streams.length > 0" class="stream-list">
       <Stream v-for="_stream in streams" :key="_stream.id" v-bind="_stream" />
@@ -12,7 +25,7 @@
 
     <button 
       class="load-more" 
-      v-if="offset >= 0" 
+      v-if="streams.length > 0 && offset >= 0" 
       v-on:click="getStreams"
     >{{isLoadingMore ? 'Loading...' : 'Load more'}}</button>
   
@@ -24,20 +37,30 @@
   import Stream       from '../boxes/Stream'
   import CategoryIcon from '../icons/CategoryIcon'
 
-  import fetchStreams from '../../functions/fetch-streams'
+  import fetchStreams      from '../../functions/fetch-streams'
+  import fetchCategory     from '../../functions/fetch-category'
+  import checkFollowStatus from '../../functions/check-follow-status'
+  import follow            from '../../functions/follow'
+  import unfollow          from '../../functions/unfollow'
 
   export default {
     name: 'Category',
 
     components: {Loading, Stream, CategoryIcon},
 
+    props: ['username'],
+
     data: function() {
       return {
-        title:         'Category',
         isLoading:     true,
         isLoadingMore: false,
         streams:       [],
         offset:        0,
+        isFollowing:   false,
+        followBtnText: 'Follow',
+        category: {
+          name: 'Category'
+        }
       }
     },
 
@@ -45,26 +68,83 @@
       getStreams: async function() {
         this.isLoadingMore = true
 
-        const category = this.$route.params.name.toLowerCase()
-        const { offset } = this
-
         const streams = 
-          await fetchStreams({category, offset})
+          await fetchStreams({category: this.category.name, offset: this.offset})
             .then(streams => streams.filter(_stream => !this.streams.find(existing => existing.id == _stream.id)))
 
         this.streams       = this.streams.concat(streams)
         this.offset        = streams.length >= 100 ? this.offset + 100 : -1
         this.isLoadingMore = false
+      },
+
+      toggleFollow: async function() {
+        this.followBtnText = 'Loading...'
+
+        if (this.isFollowing) {
+          const success = await unfollow({username: this.username, categoryName: this.category.name})
+          this.isFollowing = !success
+          this.followBtnText = 'Follow'
+        } else {
+          const success = await follow({username: this.username, categoryName: this.category.name})
+          this.isFollowing = success
+          this.followBtnText = 'Following'
+        }
+      },
+
+      mouseOver: function() {
+        this.followBtnText = this.isFollowing ? 'Unfollow' : 'Follow'
+      },
+
+      mouseOut: function() {
+        this.followBtnText  = this.isFollowing ? 'Following' : 'Follow'
       }
     },
 
     mounted: async function() {
-      if (this.$route.params.name) {
-        await this.getStreams()
-
-        this.title     = this.$route.params.name
+      if (!this.$route.params.name) {
         this.isLoading = false
+        return
       }
+
+      const category = await fetchCategory({name: this.$route.params.name})
+      
+      if (category) {
+        this.category      = category
+        this.isFollowing   = await checkFollowStatus({username: this.username, categoryName: category.name})
+        this.followBtnText = this.isFollowing ? 'Following' : 'Follow'
+        await this.getStreams()
+      } else {
+        this.category.name = this.$route.params.name
+      }
+
+      this.isLoading = false
     }
   }
 </script>
+
+<style scoped>
+  .category {
+    display: grid;
+    grid-gap: 2em;
+    align-content: start;
+  }
+
+  header {
+    display: flex;
+    align-items: flex-end;
+    border-bottom: 1px solid var(--color-border);
+  }
+
+  .title {
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+
+  .category-menu {
+    margin: 0 0 .25em 1em;
+  }
+
+  .cta {
+    width: 86px;
+  }
+</style>
---
+++ src/functions/fetch-category.js
@@ -0,0 +1,29 @@
+export default async function fetchCategory({ id, name }) {
+
+  let uri = 'https://api.twitch.tv/helix/games'
+
+  if (id) {
+    uri += `?id=${id}`
+  } else if (name) {
+    uri += `?name=${name}`
+  }
+
+  const res = await fetch(uri, {
+    headers: {
+      'Client-ID': process.env.CLIENT_ID,
+    }
+  })
+
+  if (!res.ok) return
+
+  const category = await res.json()
+
+  if(category.data.length <= 0) return
+
+  return {
+    id:     category.data[0].id,
+    name:   category.data[0].name,
+    boxArt: category.data[0].box_art_url,
+  }
+
+}

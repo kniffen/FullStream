afc3db2148d1604ffef156be6560730d7995631f
Kniffen <knifftech@gmail.com>
2020-05-27 09:01:09 +0200

    Update Channel.vue

--- src/components/views/Channel.vue
+++ src/components/views/Channel.vue
@@ -2,11 +2,10 @@
   <div class="channel">
     <header>
       <span class="title">
-        <ChannelIcon v-bind:name="name" />{{name}}
+        <ChannelIcon :name="name" />{{name}}
       </span>
     
       <div class="channel-menu">
-        <router-link v-bind:to="`/channel/${name}`">Info</router-link>
         <router-link v-bind:to="`/channel/${name}/videos/archive`">Archive</router-link>
         <router-link v-bind:to="`/channel/${name}/videos/highlight`">Highlights</router-link>
         <router-link v-bind:to="`/channel/${name}/videos/upload`">Uploads</router-link>
@@ -16,19 +15,25 @@
       </div>
     </header>
 
-    <slot></slot>
+    <ChannelInfo v-bind="{name, userID}"></ChannelInfo>
+
+    <div class="contents">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
   import ChannelIcon from '../icons/ChannelIcon'
 
+  import ChannelInfo from  './channel/Info'
+
   export default {
     name: 'Channel',
 
-    components: {ChannelIcon},
+    components: {ChannelIcon, ChannelInfo},
 
-    props: ['name'],
+    props: ['name', 'userID'],
   }
 </script>
 
@@ -60,5 +65,26 @@
 
   .channel-menu .router-link-exact-active {
     color: var(--color-blue);
+  }
+
+  @media (min-width: 768px) {
+    .channel {
+      grid-template-columns: 320px 1fr;
+    }
+
+    header {
+      display: grid;
+      grid-template-columns: 320px 1fr;
+      grid-gap: 2em;
+      grid-column: 1 / 3;
+    }
+
+    .channel-menu {
+      margin: 0 0 .25em;
+    }
+
+    .contents {
+      grid-column: 2 / 3;
+    }
   }
 </style>
